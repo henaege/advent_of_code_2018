a81c44a670194a5609e0e44c7d2343ef5cd12c05
Drew Tolliver <drew.tolliver@cfacorp.com>
2018-12-04 17:03:23 -0500

    adding day 2 part 2

--- day2_2.py
+++ day2_2.py
@@ -1,7 +1,24 @@
+# Confident that your list of box IDs is complete, you're ready to find the boxes full of prototype fabric.
+#
+# The boxes will have IDs which differ by exactly one character at the same position in both strings. For example, given the following box IDs:
+#
+# abcde
+# fghij
+# klmno
+# pqrst
+# fguij
+# axcye
+# wvxyz
+# The IDs abcde and axcye are close, but they differ by two characters (the second and fourth). However, the IDs fghij and fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+#
+# What letters are common between the two correct box IDs? (In the example above, this is found by removing the differing character from either ID, producing fgij.)
+
 box_id_set = set(line.strip() for line in open('input2.txt'))
 input_file = open('input2.txt')
 
 box_id_list = input_file.readlines()
 
 input_file.close()
+
+
 
